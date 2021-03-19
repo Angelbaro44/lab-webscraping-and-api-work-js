81bc10e057a532e68c0c044c8e7e2d9548cb9330
Angel <angelbaro44@icloud.com>
2021-03-19 16:23:05 -0400

    upload 1

---
+++ index.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+async function getFrom (url) {
+  const browser = await puppeteer.launch({headless:false, slowMo:500});
+  const page = await browser.newPage();
+  await page.goto(url);
+let data = await page.evaluate(()=>{
+  let title = document.querySelector("#post-858 > div > p:nth-child(3) > strong").innerHTML;
+  let textBlock1 =document.querySelector("#post-858 > div > p:nth-child(3) > strong").innerHTML
+  let textBlock2 =document.querySelector("#post-858 > div > p:nth-child(8)").innerHTML
+  let textBlock3 =document.querySelector("#post-858 > div > p:nth-child(9)").innerHTML
+  let textBlock4 =document.querySelector("#post-858 > div > p:nth-child(10)").innerHTML
+  return {
+    title,
+    textBlock1,
+    textBlock2,
+    textBlock3,
+    textBlock4
+  }
+} );
+
+
+console.log(data);
+
+  await browser.close(); 
+};
+
+
+getFrom('https://miamiinmiami.com/2020/09/06/ahdriana-amandi-miami-as-text/');
